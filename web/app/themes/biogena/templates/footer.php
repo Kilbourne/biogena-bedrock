dbb275c3d1afff1cc71c7ca2ab4937cdfc0a58ed
Kilbourne <luca.castellone@gmail.com>
2015-11-30 12:28:49 +0100

    dsd

--- web/app/themes/biogena/templates/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
- <div class="container">
-   <div class="copyr">
-    2015  - Valetudo S.r.l. / Divisione Biogena
-    </div><div class="privacy"><a href="" title="">Condizioni d'uso</a> - <a href="" title="">Politica di privacy</a> - <a href="" title="">Cookies</a>
-    </div><div class="contatti"><a href="" title="">Contatti</a> - <a href="" title="FAQ">FAQ</a> - <a href="" title="Area Riservata">Area Riservata</a> - <a href="" title="Lavora con noi">Lavora con noi</a>
-    </div><div class="social"><a href=""><span class='fb'></span></a><a href=""><span class='twt'></span></a><a href=""><span class='insta'></span></a><a href=""><span class='yt'></span></a> </div>
- </div>
-</footer>
